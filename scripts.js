ac22261c709857da6286800fc6b7bb6d9ca4897b
gastonnrodriguez <gastonnrodriguez@gmail.com>
2022-10-05 00:35:05 -0300

    las tarjetas quedaron dinamicas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const query = document.getElementById("query");
+/*const query = document.getElementById("query");
 
 query.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -28,9 +28,9 @@ const getError = msg => {
   message.classList.add("error");
   message.innerText = msg;
   moviesDeck.appendChild(message);
-};
-
-const getStores = async () => {
+};*/
+//async cuando sea api
+const getStores = () => {
   const stores = [
     {
       days: ["martes", "jueves"],
@@ -43,7 +43,7 @@ const getStores = async () => {
       picture: "./puesto.jpg",
     },
     {
-      day: ["martes", "miercoles", "sabado"],
+      days: ["martes", "miercoles", "sabado"],
       category: "Queseria",
       name: "El quesito",
       description:
@@ -74,84 +74,108 @@ const getStores = async () => {
       picture: "./puesto.jpg",
     },
   ];
-  try {
-    const storesList = document.getElementById("storesList");
-    /*if (result.Response === "False") {
+
+  const storesList = document.getElementById("storesList");
+  /*if (result.Response === "False") {
       getError(result.Error);
     } else emptyDeck();*/
-    stores.map(store => {
-      //creating column to contain the card
-      const col = document.createElement("div");
-      col.classList.add("col");
-      //creating the card container
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.classList.add("shadow-lg");
-      col.appendChild(card);
-      //adding an image
-      const storImage = document.createElement("img");
-      storImage.src = store.picture;
-      card.appendChild(storImage);
-      //creating card body
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-      card.appendChild(cardBody);
-      //Store label
-      const storeName = document.createElement("h5");
-      storeName.appendChild(store.name);
-      card.appendChild(storeName);
-      //Store info
-      //Working days
-      //falta iterar en los dias
-      const workingDays = document.createElement("h6");
-      workingDays.classList.add("card-subtitle");
-      workingDays.classList.add("mb-2");
-      workingDays.classList.add("text-muted");
-      //working days icon
-      const dayIcon = document.createElement("i");
-      dayIcon.classList.add("fa-solid");
-      dayIcon.classList.add("fa-calendar");
-      dayIcon.appendChild(store.days[0]);
-      //Store category
-      const storeCategory = document.createElement("h6");
-      storeCategory.classList.add("card-subtitle");
-      storeCategory.classList.add("mb-2");
-      storeCategory.classList.add("text-muted");
-      //Store category icon
-      const categoryIcon = document.createElement("i");
-      categoryIcon.classList.add("fa-solid");
-      categoryIcon.classList.add("fa-apple-whole");
-      categoryIcon.appendChild(store.category);
-      //Store description
-      const storeDescription = document.createElement("p");
-      storeDescription.classList.add("card-text");
-      storeDescription.appendChild(store.description);
-      //Store services
+  stores.map(store => {
+    //creating column to contain the card
+    const col = document.createElement("div");
+    col.classList.add("col");
+    //creating the card container
+    const card = document.createElement("div");
+    card.classList.add("card", "shadow-lg");
+    col.appendChild(card);
+    //adding an image
+    const storeImage = document.createElement("img");
+    storeImage.classList.add("card-img-top");
+    storeImage.src = store.picture;
+    card.appendChild(storeImage);
+    //creating card body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    card.appendChild(cardBody);
+    //Store label
+    const storeName = document.createElement("h4");
+    storeName.classList.add("card-title");
+    storeName.textContent = store.name;
+    cardBody.appendChild(storeName);
+    //Store info
+
+    //Store category
+    const storeCategory = document.createElement("h6");
+    storeCategory.classList.add("card-subtitle", "mb-2", "text-muted");
+
+    //Store category icon
+    const categoryIcon = document.createElement("i");
+    categoryIcon.classList.add("fa-solid", "fa-apple-whole");
+    categoryIcon.textContent = " " + store.category;
+    storeCategory.appendChild(categoryIcon);
+    cardBody.appendChild(storeCategory);
+    //Working days
+    //falta iterar en los dias
+    const workingDays = document.createElement("h6");
+    workingDays.classList.add("card-subtitle", "mb-2", "text-muted");
+
+    //working days icon
+    const dayIcon = document.createElement("i");
+    dayIcon.classList.add("fa-solid", "fa-calendar");
+    store.days.map(day => {
+      dayIcon.textContent += " " + day;
     });
 
-    result.Search.forEach(movie => {
-      //creating card
-      const card = document.createElement("div");
-      card.classList.add("movieCard");
-      //adding poster
-      const poster = document.createElement("img");
-      poster.src = movie.Poster;
-      card.appendChild(poster);
-      //Adding Title and Year
-      const cardText = document.createElement("div");
-      cardText.classList.add("info");
-      const title = document.createElement("h2");
-      const year = document.createElement("h4");
-      title.innerHTML = movie.Title;
-      year.innerHTML = movie.Year;
-      cardText.appendChild(title);
-      cardText.appendChild(year);
-      card.appendChild(cardText);
-      //adding movie card to deck
-      moviesDeck.appendChild(card);
-    });
-  } catch (error) {
-    console.log(error);
-    getError(error);
-  }
+    workingDays.appendChild(dayIcon);
+    cardBody.appendChild(workingDays);
+
+    //Store description
+    const storeDescription = document.createElement("p");
+    storeDescription.classList.add("card-text");
+    storeDescription.textContent = store.description;
+    cardBody.appendChild(storeDescription);
+    //Store services
+    //whatsapp despues hacerlo dinamico true false
+    if (store.whatsapp.length > 0) {
+      const badgeWhatsapp = document.createElement("span");
+      badgeWhatsapp.classList.add("badge", "rounded-pill", "whatsapp");
+      const whatsappIcon = document.createElement("i");
+      whatsappIcon.classList.add("fa-brands", "fa-whatsapp");
+      badgeWhatsapp.appendChild(whatsappIcon);
+      const textWhatsapp = document.createTextNode(" Whatsapp");
+      badgeWhatsapp.appendChild(textWhatsapp);
+      const whatsappLink = document.createElement("a");
+      whatsappLink.href = `https://wa.me/${store.whatsapp}`;
+      whatsappLink.appendChild(badgeWhatsapp);
+      cardBody.appendChild(whatsappLink);
+    }
+
+    if (store.pos) {
+      const badgeCards = document.createElement("span");
+      badgeCards.classList.add("badge", "rounded-pill", "text-dark", "cards");
+      const cardsIcon = document.createElement("i");
+      cardsIcon.classList.add("fa-regular", "fa-credit-card");
+      badgeCards.appendChild(cardsIcon);
+      const textCards = document.createTextNode(" Tarjetas");
+      badgeCards.appendChild(textCards);
+      cardBody.appendChild(badgeCards);
+    }
+
+    if (store.delivery) {
+      const badgeDelivery = document.createElement("span");
+      badgeDelivery.classList.add(
+        "badge",
+        "rounded-pill",
+        "bg-primary",
+        "delivery"
+      );
+      const deliveryIcon = document.createElement("i");
+      deliveryIcon.classList.add("fa-solid", "fa-truck");
+      badgeDelivery.appendChild(deliveryIcon);
+      const textDelivery = document.createTextNode(" Delivery");
+      badgeDelivery.appendChild(textDelivery);
+      cardBody.appendChild(badgeDelivery);
+    }
+    storesList.appendChild(col);
+  });
 };
+getStores();
